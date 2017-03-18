72657c291075ffd2919b8dcf59a5579eb3e539c1
Maxim Khatskevich <maxim@khatskevi.ch>
2017-03-18 16:23:02 +0100

    Prepared to start using MKHProjGen + Struct

---
+++ Carthage/Checkouts/MKHProjGen/Src/Manager.swift
@@ -0,0 +1,38 @@
+//
+//  Manager.swift
+//  MKHProjGen
+//
+//  Created by Maxim Khatskevich on 3/16/17.
+//  Copyright © 2017 Maxim Khatskevich. All rights reserved.
+//
+
+public
+enum Manager
+{
+    public
+    static
+    func prepareSpec(
+        version: String = "1.2.0",
+        for project: Project
+        ) -> String
+    {
+        let rawSpec: RawSpec
+        
+        //===
+        
+        switch version
+        {
+            case "1.2.0":
+                rawSpec = Spec_1_2_0.generate(for: project)
+            
+            default:
+                rawSpec = Spec_1_2_0.generate(for: project)
+        }
+        
+        //===
+        
+        return rawSpec
+            .map { "\(Spec.ident($0))\($1)" }
+            .joined(separator: "\n")
+    }
+}
